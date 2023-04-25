d600017d3ca5d57457d3c1cdc376ff3171257b1b
Karina <105536477+KarinaShupik@users.noreply.github.com>
2023-04-25 11:27:59 +0300

    Graphics

---
+++ Graphic/Graphic/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Graphic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_a = new System.Windows.Forms.TextBox();
+            this.txt_b = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_r = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(254, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Намалювати графік";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введіть перше число:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введіть друге число:";
+            // 
+            // txt_a
+            // 
+            this.txt_a.Location = new System.Drawing.Point(185, 36);
+            this.txt_a.Name = "txt_a";
+            this.txt_a.Size = new System.Drawing.Size(102, 22);
+            this.txt_a.TabIndex = 3;
+            // 
+            // txt_b
+            // 
+            this.txt_b.Location = new System.Drawing.Point(185, 69);
+            this.txt_b.Name = "txt_b";
+            this.txt_b.Size = new System.Drawing.Size(102, 22);
+            this.txt_b.TabIndex = 4;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(355, 36);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(733, 372);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Введіть радіус:";
+            // 
+            // txt_r
+            // 
+            this.txt_r.Location = new System.Drawing.Point(185, 100);
+            this.txt_r.Name = "txt_r";
+            this.txt_r.Size = new System.Drawing.Size(100, 22);
+            this.txt_r.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 450);
+            this.Controls.Add(this.txt_r);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.txt_b);
+            this.Controls.Add(this.txt_a);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_a;
+        private System.Windows.Forms.TextBox txt_b;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_r;
+    }
+}
+
---
+++ Graphic/Graphic/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Graphic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Очищення графіку
+            chart1.Series.Clear();
+
+            // Введення коефіцієнтів з клавіатури
+            double x0 = Convert.ToDouble(txt_a.Text);
+            double y0 = Convert.ToDouble(txt_b.Text);
+            double R = Convert.ToDouble(txt_r.Text);
+
+            // Область визначення функції
+            double[] t = new double[500];
+            double dt = 2 * Math.PI / 500;
+            for (int i = 0; i < 500; i++)
+            {
+                t[i] = i * dt;
+            }
+
+            // Параметричне рівняння кривої
+            double[] x = new double[500];
+            double[] y = new double[500];
+            for (int i = 0; i < 500; i++)
+            {
+                x[i] = x0 + R * Math.Cos(t[i]);
+                y[i] = y0 + R * Math.Sin(t[i]);
+            }
+
+            // Побудова графіку
+            Series series = chart1.Series.Add("Функція");
+            series.ChartType = SeriesChartType.Line;
+            for (int i = 0; i < 500; i++)
+            {
+                series.Points.AddXY(x[i], y[i]);
+            }
+
+            // Налаштування вісей та заголовку
+            chart1.ChartAreas[0].AxisX.Minimum = x0 - R;
+            chart1.ChartAreas[0].AxisX.Maximum = x0 + R;
+            chart1.ChartAreas[0].AxisX.Title = "x";
+            chart1.ChartAreas[0].AxisY.Minimum = y0 - R;
+            chart1.ChartAreas[0].AxisY.Maximum = y0 + R;
+            chart1.ChartAreas[0].AxisY.Title = "y";
+            chart1.Titles.Clear();
+            chart1.Titles.Add("Графік функції");
+        }
+
+        
+    }
+
+}
+
